806a5032c7f967c850079214c1ac925db8f03013
U-HW\m.owais <m.owais@HW-PD-32.hw.com.local>
2023-01-12 18:02:28 +0500

    Initial commit
    Generated by create-expo-app 1.1.5.

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LandingScreen from './components/auth/Landing';
+
+const Stack = createNativeStackNavigator()
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="LandingScreen" component={LandingScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
