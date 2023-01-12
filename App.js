a9b8ed672ca17eec18d06872894f8be7b2c8de84
U-HW\m.owais <m.owais@HW-PD-32.hw.com.local>
2023-01-12 18:18:14 +0500

    first commit

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LandingScreen from './components/auth/Landing';
+import Landing from './components/auth/Landing';
 
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="LandingScreen" component={LandingScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/auth/Landing.js
+++ components/auth/Landing.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Text, View, Button } from 'react-native'
 
-export default function LandingScreen({ navigation }) {
+export default function Landing({ navigation }) {
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>Landing
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text>Landing</Text>
     <Button title="Register" onPress={() => navigation.navigate("Register")} />
     <Button title="Login" onPress={() => navigation.navigate("Login")} />
     </View>
